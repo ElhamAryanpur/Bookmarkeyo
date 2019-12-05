130ef64c99111d6da971efc5b43868f8a54b1465
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-12-05 14:42:21 +0430

    small change made

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ else {
     }
 }
 
+// Array Remove - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
     var rest = this.slice((to || from) + 1 || this.length);
     this.length = from < 0 ? this.length + from : from;
